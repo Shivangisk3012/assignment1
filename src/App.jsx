a5a274e8f8c8fa9e9a59e1b53dd80966ac13689f
Shivangisk3012 <21mca058@jssaten.ac.in>
2023-05-13 10:14:11 +0530

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import Homebar from "./groups/Homebar";
 import Invitegroup from "./groups/Invitegroup";
 import TaskDate from "./groups/TaskDate";
 import Deadline from "./groups/Deadline";
 import TaskTopic from "./groups/TaskTopic";
 import TaskTopic2 from "./groups/TaskTopic2";
 import ReleatedDocument from "./groups/ReleatedDocument";
-import "./App.css"
+import "./Styles/App.css"
+import Department from "./groups/Department";
+
 
 function App() {
   return (
-
+    <>
     <div className="App">
+      <Homebar/>
       <div className="maincontainer">
-        <TaskTopic/>
-        <TaskTopic2/>
+        <TaskTopic />
+        <TaskTopic2 />
         <TaskDate />
         <Deadline />
-        
+        <Department/>
         <Invitegroup />
-        <ReleatedDocument/>
+        <ReleatedDocument />
       </div>
     </div>
+    </>
   );
 }
 
